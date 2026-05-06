75b10e8bba846de55233ddb5bbe57118ce158bc0
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-06 14:54:52 +0300

    feat: add initial project files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+      <div className="">
+        <h1 className="">Expense tracker</h1>
+        <h5 className="">Start taking control of your finances and life. Record, categorize and analyze your spending.</h5>
+      </div>
+    </div>
+  )
+}
+
+export default App
