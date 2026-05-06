0c4a5d2f1e1e08923e1a3ecea8260a264e22f418
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-06 15:09:14 +0300

    feat: add form jsx file

---
+++ src/components/form.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function form() {
+  return (
+    <form className="">
+        <h3 className="">
+            Add Expense
+        </h3>
+
+        <h5 className="">
+            Enter your expense details below
+        </h5>
+
+        <input type="text" placeholder="Enter expense name" className="" />
+        <input type="text" placeholder="Enter expense description" className="" />
+        <input type="text" placeholder="Enter expense category" className="" />
+        <input type="number" placeholder="Enter amount" className="" />
+        <input type="date" placeholder="dd/mm/yy" className="" />
+
+        <button className=""></button>
+    </form>
+  )
+}
