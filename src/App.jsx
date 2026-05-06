6d9a8ffdb0497e3803fd8c5e0749d9781e4583af
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-06 15:00:40 +0300

    feat: add app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="">
-      <div className="">
+    <div className=""> /**Container */
+      <div className=""> /**Heading */
         <h1 className="">Expense tracker</h1>
         <h5 className="">Start taking control of your finances and life. Record, categorize and analyze your spending.</h5>
+      </div>
+
+      <div className="">
+        <div className=""></div> /**Form */
+        <div className=""></div> /**Expense filters & list */
       </div>
     </div>
   )
